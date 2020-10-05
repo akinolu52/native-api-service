49a809ba22234320c741ebc4f1caa871f45d9b34
Akinyemi Olumide Emmannuel <akinolu52@gmail.com>
2020-10-05 01:02:06 +0100

    project setup

---
+++ README.md
@@ -0,0 +1,56 @@
+# js-api-service
+
+A simple way to make API request from the client end
+
+## Installation
+
+Use the package manager [npm](https://nodejs.org/en/download/) to install js-api-service.
+
+```bash
+npm install js-api-service
+```
+
+## Explanation
+
+***ApiService.request(method, path, callback, errorCallback, payload, exectuteWhileLoading);***
+
+1. **method**: this is any of the HTTP request method - get, delete, head, options, posst, put, patch,
+2. **path**: this is the relative path to the needed resource - '/user'
+3. **callback**: this is a function that gets executed when the request is successful - (response) => console.log('response from API -> ', response)  
+4. **errorCallback**: this is a error function that gets executed when the request fails - (error) => console.log('error from API -> ', error)  
+5. **payload (optional)**: this is an object that needs you to send data to the API - { key: value }
+6. **exectuteWhileLoading (optional)**: this is a function that gets executed while the request is loading - NB: you can show a toast or upload progress
+
+
+## Usage
+
+```javascript
+import { ApiService } from 'js-api-service';
+
+const userApiService = new ApiService('https://jsonplaceholder.typicode.com');
+// or
+// const userApiService = new ApiService('https://jsonplaceholder.typicode.com', 'Bearer **token**');
+// or
+// const userApiService = new ApiService('https://jsonplaceholder.typicode.com', 'Bearer **token**', 'application/json');
+userApiService.request(
+    'get', 
+    '/users',
+    response => {
+        console.log(response);
+        return response;
+    },
+    error => {
+        console.log(error);
+        return error;
+    },
+    null,
+    console.log('Loading all users...'),
+);
+
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[GNU](https://www.gnu.org/licenses)
---
+++ index.ts
@@ -0,0 +1,66 @@
+export type Method =
+    | 'get' | 'GET'
+    | 'delete' | 'DELETE'
+    | 'head' | 'HEAD'
+    | 'options' | 'OPTIONS'
+    | 'post' | 'POST'
+    | 'put' | 'PUT'
+    | 'patch' | 'PATCH'
+
+export class ApiService {
+    private baseURL: string;
+    private token: string;
+    private contentType: string;
+
+    constructor(baseURL: string, token?: string, contentType?: string) {
+
+        this.baseURL = baseURL;
+        this.token = token;
+        this.contentType = contentType;
+    }
+
+    async request(
+        method: Method,
+        path: string,
+        callback: any,
+        errorCallback: any,
+        payload?: object,
+        exectuteWhileLoading?: any,
+    ) {
+        if (exectuteWhileLoading) exectuteWhileLoading();
+        const requestMethod = method.toLowerCase();
+
+        if (requestMethod === 'get' || requestMethod === 'options') {
+            return await fetch(`${this.baseURL}${path}`, {
+                method,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json' || this.contentType,
+                    'Access-Control-Allow-Methods': '*',
+                    'Access-Control-Allow-Origin': this.baseURL,
+                    'Authorization': this.token,
+                },
+            })
+                .then(response => response.json())
+                .then(response => callback(response))
+                .catch(errorCallback);
+
+        } else {
+            return await fetch(`${this.baseURL}${path}`, {
+                method,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json' || this.contentType,
+                    'Access-Control-Allow-Methods': '*',
+                    'Access-Control-Allow-Origin': this.baseURL,
+                    'Authorization': this.token,
+                },
+                body: JSON.stringify(payload)
+            })
+                .then(response => response.json())
+                .then(response => callback(response))
+                .catch(errorCallback);
+
+        }
+    }
+}
